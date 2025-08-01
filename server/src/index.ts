56d8a8611855075d6868264420762b9ff9af83af
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2025-08-01 19:15:06 +0530

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { createBot } from "./bot";
 import apiRouter from "./api/routes/routes";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", apiRouter);
 
