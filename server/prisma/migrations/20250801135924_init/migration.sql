165c6ad9a9e1186835ae6b76bfce9156a85ed3d1
Amitanshu Sahu <amitanshusahu.official@gmail.com>
2025-08-01 19:29:54 +0530

    migration

--- server/prisma/migrations/20250801135924_init/migration.sql
+++ server/prisma/migrations/20250801135924_init/migration.sql
@@ -16,11 +16,26 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" TEXT NOT NULL DEFAULT 'system',
+    "updatedBy" TEXT NOT NULL DEFAULT 'system',
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Task" (
     "id" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "completed" BOOLEAN NOT NULL DEFAULT false,
     "priority" "TaskPriority" NOT NULL DEFAULT 'P3',
+    "projectId" TEXT,
+    "remarks" TEXT,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -48,6 +63,7 @@ CREATE TABLE "AssignTask" (
 CREATE TABLE "Note" (
     "id" TEXT NOT NULL,
     "content" TEXT NOT NULL,
+    "projectId" TEXT,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -61,6 +77,7 @@ CREATE TABLE "Note" (
 CREATE TABLE "AuthKey" (
     "id" TEXT NOT NULL,
     "content" TEXT NOT NULL,
+    "projectId" TEXT,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -70,11 +87,54 @@ CREATE TABLE "AuthKey" (
     CONSTRAINT "AuthKey_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Highlight" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "projectId" TEXT,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" TEXT NOT NULL DEFAULT 'system',
+    "updatedBy" TEXT NOT NULL DEFAULT 'system',
+
+    CONSTRAINT "Highlight_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Reminder" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "dateTime" TIMESTAMP(3) NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" TEXT NOT NULL DEFAULT 'system',
+    "updatedBy" TEXT NOT NULL DEFAULT 'system',
+
+    CONSTRAINT "Reminder_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_name_key" ON "Project"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "AssignTask_taskId_key" ON "AssignTask"("taskId");
 
 -- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "AssignTask" ADD CONSTRAINT "AssignTask_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AuthKey" ADD CONSTRAINT "AuthKey_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Highlight" ADD CONSTRAINT "Highlight_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE SET NULL ON UPDATE CASCADE;
